3d574ef04cf3949e6bda76018f906c170c83e1b7
Sebastian <newerdim@onet.pl>
2021-07-20 12:16:09 +0200

    Add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: "sign-up", component: SignUpComponent}
+  { path: "sign-up", component: SignUpComponent},
+  { path: "log-in", component: LogInComponent},
 ];
 
 @NgModule({
