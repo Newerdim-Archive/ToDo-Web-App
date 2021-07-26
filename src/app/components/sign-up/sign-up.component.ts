f6b2220d5418b8adea4d37bd573a46748a80a027
Sebastian <newerdim@onet.pl>
2021-07-26 18:27:02 +0200

    Add hover effect on "a" tag

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  SocialAuthService,
+  SocialUser,
+} from 'angularx-social-login';
 import { ErrorResponse } from 'src/app/interfaces/error-response';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
-
   socialUser!: SocialUser;
   errorMessage!: string;
   buttonLoading = false;
@@ -18,24 +21,23 @@ export class SignUpComponent implements OnInit {
   constructor(
     private socialAuthService: SocialAuthService,
     private authService: AuthService,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.socialAuthService.authState.subscribe((user) => {
-
       if (user) {
         this.buttonLoading = true;
 
-        this.authService.signUpWithGoogle(user.idToken)
-          .subscribe(
-            () => {
-              this.router.navigate(['/']);
-            },
-            (error: ErrorResponse) => {
-              this.errorMessage = error.message;
-              this.buttonLoading = false;
-            }
-          )
+        this.authService.signUpWithGoogle(user.idToken).subscribe(
+          () => {
+            this.router.navigate(['/']);
+          },
+          (error: ErrorResponse) => {
+            this.errorMessage = error.message;
+            this.buttonLoading = false;
+          }
+        );
       }
     });
   }
@@ -43,9 +45,9 @@ export class SignUpComponent implements OnInit {
   signUpWithGoogle(): void {
     this.errorMessage = '';
     this.buttonLoading = true;
-    
+
     this.socialAuthService
       .signIn(GoogleLoginProvider.PROVIDER_ID)
-      .catch(() => this.buttonLoading = false);
+      .catch(() => (this.buttonLoading = false));
   }
 }
